f2dec22f7358c57d1af515f8226a2742873bd326
Leniton <lenitoncarneiro@gmail.com>
2023-07-27 08:00:38 -0300

    initial comit

---
+++ Assets/Package/Runtime/Scripts/NumberUtil.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace NumberUtilities
+{
+    public class NumberUtil
+    {
+        public static bool ContainsBytes(int value, int comparingValue) => (comparingValue ^ value) == (comparingValue - value);
+        public static int Invert(int value, int maxValue) => Mathf.Abs(value - maxValue);
+        public static float Invert(float value, float maxValue) => Mathf.Abs(value - maxValue);
+    }
+}
