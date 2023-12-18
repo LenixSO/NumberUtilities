24700a4b1bf03f97e9f51f6814f571d5a3370426
Leniton <lenitoncarneiro@gmail.com>
2023-12-18 15:50:41 -0300

    package

---
+++ Assets/Tests/TestScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leniton.NumberUtilities;
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
