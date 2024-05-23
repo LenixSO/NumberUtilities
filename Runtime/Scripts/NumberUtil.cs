446607499b99bb1da457d9597f6d0e3a30a57bd3
Leniton <lenitoncarneiro@gmail.com>
2024-05-23 14:22:46 -0300

    commented code

--- Runtime/Scripts/NumberUtil.cs
+++ Runtime/Scripts/NumberUtil.cs
@@ -5,17 +5,36 @@ namespace Lenix.NumberUtilities
 {
     public static class NumberUtil
     {
-        public static bool ContainsBytes(int value, int comparingValue) => (comparingValue ^ value) == (comparingValue - value);
         public static int Invert(int value, int maxValue) => Mathf.Abs(value - maxValue);
         public static float Invert(float value, float maxValue) => Mathf.Abs(value - maxValue);
-        public static bool ContainsAnyBits(int value, int comparingValue)
+
+        /// <summary>
+        /// Checks if given value contains given bits
+        /// </summary>
+        /// <param name="value">The value being checked</param>
+        /// <param name="bytes">the bytes it must contain</param>
+        /// <returns>true if value contains all bytes, otherwise false</returns>
+        public static bool ContainsBytes(int value, int bytes) => (value ^ bytes) == (value - bytes);
+
+        /// <summary>
+        /// Checks if given value contains any of the given bits
+        /// </summary>
+        /// <param name="value">The value being checked</param>
+        /// <param name="bytes">the bytes it must contain</param>
+        /// <returns>true if value contains any of the bytes, otherwise false</returns>
+        public static bool ContainsAnyBits(int value, int bytes)
         {
-            int[] bits = SeparateBits(value);
+            int[] bits = SeparateBits(bytes);
             for (int i = 0; i < bits.Length; i++)
-                if (ContainsBytes(bits[i], comparingValue))
+                if (ContainsBytes(bits[i], value))
                     return true;
             return false;
         }
+        /// <summary>
+        /// Separate a value into its individual bits
+        /// </summary>
+        /// <param name="value">Value being divided</param>
+        /// <returns>An array with each bit</returns>
         public static int[] SeparateBits(int value)
         {
             List<int> bits = new List<int>();
@@ -28,21 +47,64 @@ namespace Lenix.NumberUtilities
             }
             return bits.ToArray();
         }
+
+        /// <summary>
+        /// Gives the value of a point in a sine wave given the time
+        /// </summary>
+        /// <param name="time">The point in time</param>
+        /// <param name="amplitude">The amplitude of the sine wave</param>
+        /// <param name="frequency">The frequency of the wave</param>
+        /// <param name="offset">Wave time offset</param>
+        /// <returns>A value between amplitude and -amplitude based on the parameters</returns>
         public static float SineWave(float time, float amplitude, float frequency, float offset = 0) => amplitude * (Mathf.Sin(2 * Mathf.PI * frequency * time + offset));
-        public static float NormalizedDistance(float value, float target, float range)
-        {
-            return Mathf.Clamp01(1 - Mathf.Abs(value - target) / range);
-        }
-        public static float WithinTargetDistance(float value, float min, float max)
+
+        /// <summary>
+        /// Normalizes value to given min/max range
+        /// </summary>
+        /// <param name="value">The value being normalized</param>
+        /// <param name="min">The minimum value of range</param>
+        /// <param name="max">The maximum value of range</param>
+        /// <returns>A percentage representing where the original value falls within the given range.</returns>
+        public static float NormalizeToRange(float value, float min, float max)
         {
             return (value - min) / (max - min);
         }
-        public static float DetermineAlphaValue(float value, float min, float max, float range)
+
+        /// <summary>
+        /// Calculates how far the value is from target given the range
+        /// </summary>
+        /// <param name="value">The value you want to check</param>
+        /// <param name="target">The target value to compare</param>
+        /// <param name="range">The range in which the distance is verified</param>
+        /// <returns>
+        /// A value between 0 and 1 representing how far from the target is the original value, 
+        /// where 1 means the value is equal to the target and 
+        /// 0 the value distance from target is bigger or equal to the range.
+        /// </returns>
+        public static float DistanceFromTarget(float value, float target, float range)
         {
-            float alphaValue = NormalizedDistance(value, min, range) +
-                    Mathf.Clamp01(NormalizedDistance(value, max, range) +
-                    Mathf.Ceil(NormalizedDistance(value, min + ((max - min) / 2), (max - min) / 2))) *
-                    Mathf.Ceil(Mathf.Clamp01(WithinTargetDistance(value, min, max)));
+            return Mathf.Clamp01(1 - Mathf.Abs(value - target) / range);
+        }
+
+        /// <summary>
+        /// Calculates how far the value is from the area between min and max given the range
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="min">The minimum value of area</param>
+        /// <param name="max">The maximum value of area</param>
+        /// <param name="range">The range in which the distance is verified</param>
+        /// <returns>
+        /// A value between 0 and 1 representing how far from the area is the original value, 
+        /// where 1 means the value is inside the area and 
+        /// 0 the value distance from the area is bigger or equal to the range.
+        /// Values in between represents the distance from the area based on the range
+        /// </returns>
+        public static float DistanceFromArea(float value, float min, float max, float range)
+        {
+            float alphaValue = DistanceFromTarget(value, min, range) +
+                    Mathf.Clamp01(DistanceFromTarget(value, max, range) +
+                    Mathf.Ceil(DistanceFromTarget(value, min + ((max - min) / 2), (max - min) / 2))) *
+                    Mathf.Ceil(Mathf.Clamp01(NormalizeToRange(value, min, max)));
             return alphaValue;
         }
     }
